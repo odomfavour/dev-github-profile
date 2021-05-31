412cbecdced078b2e90109ed9db888daabe661e0
ogochukwu <ogochukwu.odom@kodehauz.com>
2021-05-31 12:53:01 +0100

    removed console

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 import { graphql } from "https://cdn.skypack.dev/@octokit/graphql";
 import sum from "../test.js";
-console.log(sum)
 
 const repoLayout = document.querySelector("#repo-base");
 const fullName = document.querySelector("#name");
